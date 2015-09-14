5eee08b2d53d4a3d4335eef36db4958a040d56d5
Daniel Wichman-Buescher <danbuescher@gmail.com>
2015-09-14 13:40:17 -0400

    make models, mailers specs pass
    Former-commit-id: b1bbf4d8c0a494b52a412c8f09eef122877863b2

--- spec/factories/time_allocations.rb
+++ spec/factories/time_allocations.rb
@@ -5,12 +5,6 @@ FactoryGirl.define do
     grantholding
     timelog
     comments { Faker::Lorem.paragraph }
-    start_time Time.new(2014, 2, 5).advance(hours: 1)
-    end_time   Time.new(2014, 2, 5).advance(hours: 2)
-
-    trait :invalid do
-      start_time { timelog.start_time.change(year: 2000) }
-      end_time   { timelog.start_time.advance(hours: 1)  }
-    end 
+    hours 1.0
   end
 end
--- spec/models/time_allocation_spec.rb
+++ spec/models/time_allocation_spec.rb
@@ -1,14 +1,49 @@
 require 'rails_helper'
 
 describe TimeAllocation do
-  let(:valid_time_allocation)   { build(:time_allocation) }
-  let(:invalid_time_allocation) { build(:time_allocation, :invalid) }
-  
-  it "accepts a valid time range" do        
-    expect(valid_time_allocation).to be_valid
-  end
-  
-  it "rejects an invalid time range" do
-    expect(invalid_time_allocation).not_to be_valid
-  end
+ 	describe "factory" do
+ 		it { expect(build(:time_allocation)).to be_valid }
+ 	end
+
+ 	describe "associations" do
+ 		it { should belong_to(:grantholding) }
+ 		it { should belong_to(:timelog) }
+ 	end
+
+ 	describe "public instance methods" do
+ 		describe "#to_grant?" do
+ 			let(:organization)    { 
+ 				create(:organization) 
+ 			}
+ 			let(:grant) 			    { 
+ 				create(:grant, organization: organization) 
+ 			}
+ 			let(:other_gr) 		    { 
+ 				create(:grant, name: "Other Grant", organization: organization) 
+ 			}
+ 			let(:user) 				    { 
+ 				create(:user, organization: organization)  
+ 			}
+ 			let(:grantholding)    { 
+ 				create(:grantholding, user: user) 
+ 			}
+ 			let(:other_g) 		    { 
+ 				create(:grantholding, user: user, grant: other_gr)   
+ 			}
+ 			let(:time_allocation) { 
+ 				create(:time_allocation, grantholding: grantholding) 
+ 			}
+ 			let(:other_ta) 		    { 
+ 				create(:time_allocation, grantholding: other_g) 
+ 			}
+
+ 			it "returns true if its user's grant matches specified grant" do
+ 				expect(time_allocation.to_grant?(grant.name)).to be true
+ 			end
+
+ 			it "returns false if its user's grant doesn't match specified grant" do
+ 				expect(other_ta.to_grant?(grant.name)).to be false
+ 			end
+ 		end
+ 	end
 end 
