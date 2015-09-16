c38677d3d49fede382346a61a91ba2a529fad501
Daniel Wichman-Buescher <danbuescher@gmail.com>
2015-09-16 13:27:35 -0400

    add styling to grantholdings index
    Former-commit-id: 7b9debca715a3f6ecdb64be410d2b99c31ff062d

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,25 @@
-module UsersHelper
+module UsersHelper 
   
+  # def gravatar_for(user, options={size: 50})
+  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  #   image_tag(gravatar_url, alt: full_name(user), class: "gravatar")
+  # end
+
   # Displays a custom Gravatar image link that identifies the user.
-  def gravatar_for(user, options={size: 50})
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure/gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: full_name(user), class: "gravatar")
+  def gravatar_for(user, size=50)
+  	gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    image_tag(gravatar_url, alt: full_name(user), class: 'profile-pic')
   end
+
+  def supervision_status(user)
+    if current_user.supervises?(user)
+      "Supervisee"
+    elsif user.supervises?(current_user)
+      "Supervisor"
+    else
+      nil
+    end
+  end      
 end
